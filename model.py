b1bbaa4201762822dc69f2ff9173ad4d42d3c923
kdanna <kdanna@ucsc.edu>
2018-03-19 22:22:29 -0700

    working a3c

--- model.py
+++ model.py
@@ -18,8 +18,10 @@ class model():
             self.state_in = tf.placeholder(tf.float32, 
                     (None,) + state_shape)
             self.reward_in = tf.placeholder(tf.float32, [None])
+            self.advantage_in = tf.placeholder(tf.float32, [None])
             self.action_in = tf.placeholder(tf.float32, [None, n_actions])
             self.keep_prob = tf.placeholder(tf.float32)
+            self.explore = tf.placeholder(tf.float32)
 
         '''
         #3x3 conv2d, relu, 2x2 maxpool
@@ -51,56 +53,98 @@ class model():
             #drop = tf.nn.dropout(dense, self.keep_prob)
             drop = dense
 
-        with tf.name_scope('classify'):
+        with tf.name_scope('policy'):
             w_class = weight([n, n_actions])
             b_class = bias([n_actions])
 
-            self.predictions = tf.matmul(drop, w_class) + b_class
+            logits_class = tf.matmul(drop, w_class) + b_class
+            probs_class = tf.nn.softmax(logits_class) + 1e-8
+            logprobs_class = tf.nn.log_softmax(logits_class) + 1e-8
+            self.action = tf.argmax(probs_class, axis=1)
+
+            '''
+            #sample a 0 or 1 based on logprob in self.explore
+                #[[no explore, explore]]
+                #squeeze(shape([1,1])) --> shape([,])
+            explore_distribution = tf.log([[1 - self.explore, 
+                    self.explore]])
+            explore_sample = tf.multinomial(explore_distribution, 1)
+            explore_flag = tf.cast(tf.squeeze(explore_sample), tf.float32)
+
+            #add random explore probs per action if explore_flag = 1
+            rand = tf.random_uniform([n_actions])
+            explore_probs = probs_class + tf.multiply(explore_flag, 
+                    tf.random_shuffle(rand))
+            '''
+
+        with tf.name_scope('value'):
+            w_val = weight([n,1])
+            b_val = bias([1])
+
+            logit_val = tf.matmul(drop, w_val) + b_val
+            self.value = tf.reduce_sum(logit_val, axis=1)
+
+            '''
+            action_oh = tf.one_hot(self.action, n_actions)
+            self.value = tf.reduce_sum(tf.multiply(probs_val, action_oh), 
+                    axis=1)
+            '''
+
+        with tf.name_scope('loss'):
+            #policy loss
+            entropy = -tf.reduce_sum(probs_class * logprobs_class)
+            action_probs = tf.reduce_sum(logprobs_class * self.action_in, 
+                    [1])
+            #policy_loss = tf.reduce_sum(-(action_probs * self.advantage_in)                     + 0.01 * entropy)
+            
+            policy_loss = -tf.reduce_sum(action_probs * self.advantage_in)
+
+            #value loss
+            #value_loss = tf.reduce_sum(tf.squared_difference(self.value, 
+            #        self.reward_in))
+            value_loss = 0.05 * tf.reduce_sum(tf.squared_difference(
+                    self.value, self.reward_in))
+
+            #total loss
+            self.loss = policy_loss + 0.5 * value_loss - 0.01 * entropy
 
         with tf.name_scope('optimize'):
             self.step = tf.Variable(0, trainable=False)
-            reward_preds = tf.reduce_sum(tf.multiply(self.predictions, 
-                    self.action_in))
-            
-            self.loss = tf.reduce_mean(tf.square(
-                    self.reward_in - reward_preds))
-
             optimizer = tf.train.AdamOptimizer(1e-3)
             self.optimize = optimizer.minimize(self.loss, 
                     global_step=self.step)
 
         with tf.name_scope('summary'):
-            tf.summary.scalar('loss', self.loss)
+            tf.summary.scalar('1_total_loss', self.loss)
+            tf.summary.scalar('2_value_loss', value_loss)
+            tf.summary.scalar('3_policy_loss', policy_loss)
+            tf.summary.scalar('4_entropy', entropy)
             self.summaries = tf.summary.merge_all()
 
         self.sess = tf.Session()
         self.writer = tf.summary.FileWriter('./logs', self.loss.graph)
         self.sess.run(tf.global_variables_initializer())
 
-    def action(self, state, epsilon=0.14):
-        #returns action and corresponding value
-        action_values = self.sess.run([self.predictions], 
+    def act(self, state, explore=0.2):
+        action, value = self.sess.run([self.action, self.value], 
                 feed_dict={
                     self.state_in: [state],
                     self.keep_prob: 1.0,
-                })[0][0]
-
-        #epsilon greedy exploration
-        if np.random.random_sample() < epsilon:
-            action = np.random.randint(0, len(action_values)) 
-            return action, action_values[action]
-
-        return np.argmax(action_values), np.amax(action_values)
+                    self.explore: explore,
+                })
+        return action[0], value[0]
 
     def learn(self, batch):
-        states, actions, rewards, dones, next_states = batch
+        states, actions, rewards, values, dones, next_states = batch
         loss, _, step, summ = self.sess.run([self.loss, self.optimize,
                 self.step, self.summaries],
                 feed_dict={
                     self.state_in: states,
                     self.action_in: actions,
                     self.reward_in: rewards,
+                    self.advantage_in: values,
                     self.keep_prob: 0.5,
+                    self.explore: 0.0,
                 })
         self.writer.add_summary(summ, step)
         return loss
--- worker.py
+++ worker.py
@@ -3,9 +3,11 @@ import sys, os
 sys.dont_write_bytecode = True #remove before release
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #ignore startup messages
 
+import time
 import pandas as pd
 import numpy as np
 import gym
+import scipy.signal
 
 import misc
 
@@ -40,6 +42,7 @@ class worker():
             train_interval=20, print_interval=1000):
         misc.debug('training for %s episodes (%s steps max)' 
                 % (episodes, max_steps))
+        start_time = time.time()
         batch = replay_memory()
         n_actions = env.action_space.n
         all_stats = []
@@ -56,9 +59,10 @@ class worker():
             }
             if episode % print_interval == 0:
                 misc.debug('episode %s' % episode)
+                #self.test(env, episodes=10, max_steps=10000, records=0)
             while not done and step < max_steps:
                 #do action
-                action, _ = self.model.action(state)
+                action, value = self.model.act(state)
                 next_state, reward, done, _ = env.step(action)
                 reward = 0 if done else reward
 
@@ -67,22 +71,10 @@ class worker():
                 next_state = self.process_state(next_state)
                 action = self.to_onehot(action, n_actions)
 
-                #propogate and decay rewards backward through time
-                    #if sparse, rewards = np.linspace(last, 0)
-                    #if no rewards in whole batch
-                        #no propogation?
-                    #reward for a state is function of next x rewards
-                        #and next x values predicted for states
-
-                #FIXME: q-learning reward discount
-                    #reward = reward + discount * Q(next state)
-                _, value = self.model.action(next_state)
-                reward += 0.9 * value
-
                 #add experience to batch
-                batch.add((state, action, reward, done, next_state))
+                batch.add((state, action, reward, value, done, next_state))
 
-                if batch.size == train_interval:
+                if batch.size == train_interval or done:
                     loss = self.model.learn(batch.get())
                     stats['loss'].append(loss)
                     batch.clear()
@@ -96,11 +88,14 @@ class worker():
             stats['step'].append(step)
             stats['reward'].append(reward_sum)
                 
-            #learn on remaining steps
-            #if batch.size > 0:
-            #    loss = self.model.learn(batch.get())
             all_stats.append(stats)
         
+        end_time = time.time()
+        train_time = end_time - start_time
+        train_mins = train_time / 60
+        train_secs = train_time % 60
+        misc.debug('finished training in %0.3sm %0.3ss (%0.3ss)' % (
+                train_mins, train_secs, train_time))
         #output training stats
         #for stat in all_stats:
             #stat = pd.DataFrame(data=stat)
@@ -114,7 +109,7 @@ class worker():
         vc = lambda n: n in [int(x) for x in np.linspace(episodes-1, 0, 
                 records)] 
         #wrapper that records episodes
-        env = gym.wrappers.Monitor(env, directory=out_dir, force=False, 
+        env = gym.wrappers.Monitor(env, directory=out_dir, force=True, 
                 video_callable=vc)
         #init a dict of useful measurements
         stats = {
@@ -130,8 +125,8 @@ class worker():
                 #gym imposes internal max step anyways
             while not done: #and step < max_steps:
                 #do action
-                action, _ = self.model.action(self.process_state(state), 
-                        epsilon=0.0)
+                action, _ = self.model.act(self.process_state(state), 
+                        explore=0.0)
                 state, reward, done, _ = env.step(action)
                 
                 #update
@@ -151,6 +146,7 @@ class replay_memory():
         self.states = []
         self.actions = []
         self.rewards = []
+        self.values = []
         self.dones = []
         self.next_states = []
         self.size = 0
@@ -159,24 +155,36 @@ class replay_memory():
         self.states = []
         self.actions = []
         self.rewards = []
+        self.values = []
         self.dones = []
         self.next_states = []
         self.size = 0
 
     def add(self, experience):
-        state, action, reward, done, next_state = experience
+        state, action, reward, value, done, next_state = experience
         self.states.append(state)
         self.actions.append(action)
         self.rewards.append(reward)
+        self.values.append(value)
         self.dones.append(done)
         self.next_states.append(next_state)
         self.size += 1
 
     def get(self):
+        #calc advantage
+        reward = 0.0
+        if not self.dones[-1]:
+            reward = self.values[-1]
+        for i in range(self.size - 1, -1, -1): #reverse iterate
+            reward = self.rewards[i] + 0.99 * reward
+            self.rewards[i] = reward
+            self.values[i] = reward - self.values[i]
+
         states = np.asarray(self.states, dtype=np.float32)
         actions = np.asarray(self.actions, dtype=np.float32)
         rewards = np.asarray(self.rewards, dtype=np.float32)
+        values = np.asarray(self.values, dtype=np.float32)
         dones = np.asarray(self.dones, dtype=np.float32)
         next_states = np.asarray(self.next_states, dtype=np.float32)
-        return states, actions, rewards, dones, next_states
+        return states, actions, rewards, values, dones, next_states
 
